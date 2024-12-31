7f50720b5386d7398fdbac0bc59430655ede5597
kangkang shang <535634238@qq.com>
2024-12-31 22:52:20 +0800

    调整目录

--- docs/.vuepress/config/theme_config/sidebar/basic.js
+++ docs/.vuepress/config/theme_config/sidebar/basic.js
@@ -1,13 +1,13 @@
 module.exports = {
   title: 'JS基础相关',
   children: [{
-    title: '深拷贝和浅拷贝',
+    title: '1. 深拷贝和浅拷贝',
     path: '/basic/1'
   }, {
-    title: '内存泄漏',
+    title: '2. 内存泄漏',
     path: '/basic/2'
   }, {
-    title: '箭头函数和普通函数的区别',
+    title: '3. 箭头函数和普通函数的区别',
     path: '/basic/3'
   }]
 }
