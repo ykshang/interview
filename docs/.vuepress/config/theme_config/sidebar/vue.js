d66f6e8174004b98fda5944f26e5707fe313da8a
kangkang shang <535634238@qq.com>
2025-01-01 15:12:32 +0800

    增加vuex专题

--- docs/.vuepress/config/theme_config/sidebar/vue.js
+++ docs/.vuepress/config/theme_config/sidebar/vue.js
@@ -1,7 +1,10 @@
 module.exports = {
   title: 'Vue',
   children: [{
-    title: 'Vue的组件通信',
+    title: '1. Vue的组件通信',
     path: '/vue/1'
+  }, {
+    title: '2. Vuex专题',
+    path: '/vue/2'
   }]
 }
