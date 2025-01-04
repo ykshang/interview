e3939baa05bec101c0bfef2b8cd3340c1db0c702
kangkang shang <535634238@qq.com>
2025-01-04 23:46:38 +0800

    更新文档

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,7 +1,8 @@
 import Element from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './styles/main.css';// 引入自定义样式
-
+import LinkIcon from './componets/LinkIcon.vue'
 export default ({ Vue }) => {
   Vue.use(Element);
+  Vue.component('LinkIcon', LinkIcon)
 };
